4d958e3b53b72ce4cac1246d0f7920b37c56ab82
sujipinku07 <sujipinku07@gmail.com>
2024-09-28 20:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const line = document.getElementById("line");
+      let angle = 0;
 function rotateLine() {
         angle = (angle + 2) % 360; // Increment the angle by 2 degrees and wrap around at 360
         line.style.transform = `rotate(${angle}deg)`;
