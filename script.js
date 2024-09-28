ce18c3ac2a9bdb1508c00ee8d3d684ae77c6c177
sujipinku07 <sujipinku07@gmail.com>
2024-09-28 20:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function rotateLine() {
+        angle = (angle + 2) % 360; // Increment the angle by 2 degrees and wrap around at 360
+        line.style.transform = `rotate(${angle}deg)`;
+      }
+
+      setInterval(rotateLine, 20);//your JS code here. If required.
